577cfcc8acdf4b0fbfd58b2b2458d93b9db8565c
unknown <gongjieqi@jiuchunjiaoyu.com>
2018-05-18 16:58:59 +0800

    remove chat

--- config/chat.php
+++ config/chat.php
@@ -1,7 +1,30 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | chat app name
+    |--------------------------------------------------------------------------
+    |
+    */
+    'name'=> 'my chat app',
 
+
+    'work_count' => 4,
+
+    'registerAddress' => '127.0.0.1:1238',
+
+    'registerServer' => 'text://0.0.0.0:1238',
+
+    'lanIp' => '127.0.0.1',
+
+    'gateway_app' => 'my gateway app',
+
+    'socket_ip' => '0.0.0.0',
+
+    'start_port' => 4000,
+
+    'socket_port' => '8282',
     /*
     |--------------------------------------------------------------------------
     | User Model & Table
@@ -11,6 +34,8 @@ return [
     'user' => 'App\User',
 
     'user_table' => 'users',
+
+    'user_guard' => 'web',
     /*
     |--------------------------------------------------------------------------
     | User Model & Table
@@ -20,4 +45,6 @@ return [
     'admin' => 'App\Admin',
 
     'admin_table' => 'admins',
+
+    'admin_guard' => 'admin',
 ];
